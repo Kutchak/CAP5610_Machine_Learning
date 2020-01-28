b270a7f94f04d238f32039f5c857b45c2d2ba1b1
adam kutchak <akutchak@gmail.com>
2020-01-28 09:43:17 -0500

    Removed old comments and code

--- hw_1.py
+++ hw_1.py
@@ -1,15 +1,7 @@
 import pandas as pd
 
-# train_df = pd.read_csv('Titanic/train.csv')
-# test_df = pd.read_csv('Titanic/test.csv')
-# combine = [train_df, test_df]
-# data = pd.DataFrame(combine)
-# print(data.head(1))
 train_df = pd.read_csv('Titanic/train.csv')
-# print(train_df.head(10))
 test_df = pd.read_csv('Titanic/test.csv')
-# print(test_df.head(10))
-# combine = [train_df, test_df]
 combine = pd.concat([train_df, test_df], axis=0, sort=False)
 combine.to_csv('combined.csv', index=False)
 data = pd.read_csv('combined.csv')
