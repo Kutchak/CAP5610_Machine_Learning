bedaefbe00cf8b1d595dee6f9f642ab8ae9c57dc
adam kutchak <akutchak@gmail.com>
2020-02-18 14:40:09 -0500

    Fix main and testing

--- hw_5_main.py
+++ hw_5_main.py
@@ -21,15 +21,15 @@ from hw_5_testing import knnscore
 df = pd.read_csv('data/iris.data')
 df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'Class']
 knnscores = []
+knnkeys = []
 
 def findbestk(train_features, train_labels, test_features, test_labels):
     for i in range(100):
         knntrain(train_features, train_labels, i+1, 'topk.sav')
         knnscores.append(knnscore(test_features, test_labels, 'topk.sav'))
+        knnkeys.append(i)
         print(knnscores[i])
 
-    plt.bar(range(len(knnscores)), knnscores)
-    plt.show()
 
 def main():
     feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
@@ -104,80 +104,30 @@ def main():
     findbestk(df_train4x, df_train4y, df_test4x, df_test4y)
     findbestk(df_train5x, df_train5y, df_test5x, df_test5y)
 
-    # if not path.exists(knnfilename):
-        # knntrain(df_train1x, df_train1y, 4, knnfilename)
-    # if not path.exists(dctfilename):
-        # dcttrain(df_train1x, df_train1y, dctfilename)
+    # plt.hist(knnscores, bins=100)
+    plt.bar(knnkeys, knnscores)
+    plt.show()
 
-    # dtcscore = dctscore(df_test1x, df_test1y, dctfilename)
-    # print(dtcscore)
+    if not path.exists(knnfilename):
+        knntrain(df_train1x, df_train1y, 11, knnfilename)
+    if not path.exists(dctfilename):
+        dcttrain(df_train1x, df_train1y, dctfilename)
 
-    # neighbor.fit(df_train1x, df_train1y)
-    # clf.fit(df_train1x, df_train1y)
+    dtc_avg = dctscore(df_test1x, df_test1y, dctfilename)
+    dtc_avg = dtc_avg + dctscore(df_test2x, df_test2y, dctfilename)
+    dtc_avg = dtc_avg + dctscore(df_test3x, df_test3y, dctfilename)
+    dtc_avg = dtc_avg + dctscore(df_test4x, df_test4y, dctfilename)
+    dtc_avg = dtc_avg + dctscore(df_test5x, df_test5y, dctfilename)
+    dtc_avg = dtc_avg/5
+    print('dtc_avg', dtc_avg)
 
-    # predict1_knn = neighbor.predict(df_test1x)
-    # predict1_dct = clf.predict(df_test1x)
-
-    # # print(part1.Class)
-    # # print(neighbor.score(df_train1x, df_train1y))
-    # print('test 1 knn accuracy score', accuracy_score(predict1_knn, part1.Class))
-    # print('test 1 dct accuracy score', accuracy_score(predict1_dct, part1.Class))
-
-    # df_test2 = pd.DataFrame(data=part2)
-    # df_test2x = df_test2[feature_cols]
-
-    # neighbor.fit(df_train2x,df_train2y)
-    # clf.fit(df_train2x, df_train2y)
-
-    # predict2_knn = neighbor.predict(df_test2x)
-    # predict2_dct = clf.predict(df_test2x)
-
-    # # print(part2.Class)
-    # # print(neighbor.score(df_train2x, df_train2y))
-    # print('test 2 knn accuracy score', accuracy_score(predict2_knn, part2.Class))
-    # print('test 2 dct accuracy score', accuracy_score(predict2_dct, part2.Class))
-
-    # df_test3 = pd.DataFrame(data=part3)
-    # df_test3x = df_test3[feature_cols]
-
-    # neighbor.fit(df_train3x,df_train3y)
-    # clf.fit(df_train3x, df_train3y)
-
-    # predict3_knn = neighbor.predict(df_test3x)
-    # predict3_dct = clf.predict(df_test3x)
-
-    # # print(part3.Class)
-    # # print(neighbor.score(df_train3x, df_train3y))
-    # print('test 3 knn accuracy score', accuracy_score(predict3_knn, part3.Class))
-    # print('test 3 dct accuracy score', accuracy_score(predict3_dct, part3.Class))
-
-    # df_test4 = pd.DataFrame(data=part4)
-    # df_test4x = df_test4[feature_cols]
-
-    # neighbor.fit(df_train4x,df_train4y)
-    # clf.fit(df_train4x, df_train4y)
-
-    # predict4_knn = neighbor.predict(df_test4x)
-    # predict4_dct = clf.predict(df_test4x)
-
-    # # print(part4.Class)
-    # # print(neighbor.score(df_train4x, df_train4y))
-    # print('test 4 knn accuracy score', accuracy_score(predict4_knn, part4.Class))
-    # print('test 4 dct accuracy score', accuracy_score(predict4_dct, part4.Class))
-
-    # df_test5 = pd.DataFrame(data=part5)
-    # df_test5x = df_test5[feature_cols]
-
-    # neighbor.fit(df_train5x,df_train5y)
-    # clf.fit(df_train5x, df_train5y)
-
-    # predict5_knn = neighbor.predict(df_test5x)
-    # predict5_dct = clf.predict(df_test5x)
-
-    # # print(part5.Class)
-    # # print(neighbor.score(df_train5x, df_train5y))
-    # print('test 5 knn accuracy score', accuracy_score(predict5_knn, part5.Class))
-    # print('test 5 dct accuracy score', accuracy_score(predict5_dct, part5.Class))
+    knn_avg = knnscore(df_test1x, df_test1y, knnfilename)
+    knn_avg = knn_avg + knnscore(df_test2x, df_test2y, knnfilename)
+    knn_avg = knn_avg + knnscore(df_test3x, df_test3y, knnfilename)
+    knn_avg = knn_avg + knnscore(df_test4x, df_test4y, knnfilename)
+    knn_avg = knn_avg + knnscore(df_test5x, df_test5y, knnfilename)
+    knn_avg = knn_avg/5
+    print('knn_avg',knn_avg)
 
 if __name__=="__main__":
     main()
--- hw_5_testing.py
+++ hw_5_testing.py
@@ -20,23 +20,3 @@ def knnscore(test_features, test_labels, filename):
     loaded_model = pickle.load(open(filename, 'rb'))
     result = loaded_model.score(test_features, test_labels)
     return result
-
-    # print(neighbor.score(df_train1x, df_train1y))
-    # print('test 1 knn accuracy score', accuracy_score(predict1_knn, part1.Class))
-    # print('test 1 dct accuracy score', accuracy_score(predict1_dct, part1.Class))
-
-    # print(neighbor.score(df_train2x, df_train2y))
-    # print('test 2 knn accuracy score', accuracy_score(predict2_knn, part2.Class))
-    # print('test 2 dct accuracy score', accuracy_score(predict2_dct, part2.Class))
-
-    # print(neighbor.score(df_train3x, df_train3y))
-    # print('test 3 knn accuracy score', accuracy_score(predict3_knn, part3.Class))
-    # print('test 3 dct accuracy score', accuracy_score(predict3_dct, part3.Class))
-
-    # print(neighbor.score(df_train4x, df_train4y))
-    # print('test 4 knn accuracy score', accuracy_score(predict4_knn, part4.Class))
-    # print('test 4 dct accuracy score', accuracy_score(predict4_dct, part4.Class))
-
-    # print(neighbor.score(df_train5x, df_train5y))
-    # print('test 5 knn accuracy score', accuracy_score(predict5_knn, part5.Class))
-    # print('test 5 dct accuracy score', accuracy_score(predict5_dct, part5.Class))
